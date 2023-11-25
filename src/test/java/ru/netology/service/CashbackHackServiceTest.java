ab1a57e0326efc4809c206606f17a85c23d67d09
Bato <bato19941994@bk.ru>
2023-11-25 15:20:36 +0800

    update CashbackTest TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,13 +8,47 @@ public class CashbackHackServiceTest {
 
     @Test
     public void ShouldCasbackIfAmount1900() {
-       CashbackHackService cashbackHackService =  new CashbackHackService();
+        CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1900;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
 
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
 
+    }
+
+
+    @Test
+    public void ShouldCasbackIfAmount1800() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1800;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldCasbackIfAmount1100() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldCasbackIfAmount1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
     }
 }
